9c499429104d14d62db2a4d278e9a0fc4c3a5319
Brice-Vergnou <brice.vergnou@gmail.com>
2021-07-29 16:15:11 +0200

    added playlist search

--- get_your_model.py
+++ get_your_model.py
@@ -9,6 +9,7 @@ import urllib.parse
 
 done_getting = False
 done_cleaning = False
+bad_playlist = []
 
 print("""Hello !
           
@@ -19,14 +20,23 @@ print("""Hello !
           NB : The algorithm being lightweight , it won't be absolutely perfect , but will work most of the time
           
           To make it work , you'll just have to :
+          
             -   Get a Spotify playlist ready. This playlist will cointain 100 songs ( you can have more but only the 100 first will be used ).
                 Try to use the BEST songs in your opinion so the algorithm will perfectly know what you like
+                The 'Liked songs' playlist can't work because it is private
                 ( don't worry about privacy , we don't even have servers to store your data , it will then remain private and on your computer )
+                You will have to give us its ID
+                Just copy its link. It will look like this
+                https://open.spotify.com/playlist/[ID]?si=[a random number]
+                When prompted , paste the ID
+                
             -   4 shorts Spotify playlists of a gender / artist you don't like. Try to use different genders so the algorithm will better know
                 what you don't like.
                 And don't worry ! You don't have to create these playlist. You can just use the "This is [name of the artist]" playlists 
                 made by Spotify , or type the name of the gender you don't like and take the first playlist. 
                 Each of these playlists have to be at least 25 songs long
+                You will have to give us its name , so be precise when you type it
+                At the end , you will have a summary of the "bad" playlists to make sure they're all right
                 
             Once you have these playlists , just copy their links. They will look like this
             https://open.spotify.com/playlist/[ID]?si=[a random number]
@@ -67,22 +77,31 @@ try :
         # Get the data from the disliked playlists
         i = 1
         while i < 5 : # TODO #1 : make it easy for the user searching for a playlist
-            playlist_id = input(f"\n\nID of the 'disliked' playlist n.{i}: ")
-            playlist_id = urllib.parse.quote(playlist_id)
-            stream = os.popen(f'curl -X "GET" "https://api.spotify.com/v1/playlists/{playlist_id}/tracks?fields=items(track(id%2Cname))?limit=25" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer {token}"')
+            query = input(f"\n\nName of the 'disliked' playlist n.{i}: ")
+            query = urllib.parse.quote(query)
+            stream = os.popen(f'curl -X "GET" "https://api.spotify.com/v1/search?q={query}&type=playlist" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer {token}"')
             data = stream.read()
+            
             try :
-                data = json.loads(data)["items"]
-                songs_ids = ""
-                for track in data:
-                    songs_ids += track["track"]["id"] + ","
-                songs_ids = songs_ids[:-1]
-                stream = os.popen(f'curl -X "GET" "https://api.spotify.com/v1/audio-features?ids={songs_ids}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer {token}"')
+                data = json.loads(data)["playlists"]["items"][0]
+                bad_playlist.append(data["name"])
+                playlist_id = data["id"]
+                stream = os.popen(f'curl -X "GET" "https://api.spotify.com/v1/playlists/{playlist_id}/tracks?fields=items(track(id%2Cname))?limit=25" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer {token}"')
                 data = stream.read()
-                with open(f"bad{i}.json","w") as f:
-                    f.write(data)
-                i +=1
-                    
+                try :
+                    data = json.loads(data)["items"]
+                    songs_ids = ""
+                    for track in data:
+                        songs_ids += track["track"]["id"] + ","
+                    songs_ids = songs_ids[:-1]
+                    stream = os.popen(f'curl -X "GET" "https://api.spotify.com/v1/audio-features?ids={songs_ids}" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer {token}"')
+                    data = stream.read()
+                    with open(f"bad{i}.json","w") as f:
+                        f.write(data)
+                    i +=1
+                        
+                except KeyError:
+                    print("\n\n\nYour token has expired , create a new one : https://developer.spotify.com/console/get-several-tracks/\n\n\n")
             except KeyError:
                 print("\n\n\nYour token has expired , create a new one : https://developer.spotify.com/console/get-several-tracks/\n\n\n")
             except IndexError:
@@ -91,7 +110,8 @@ try :
         done_getting = True
         
     except KeyError:
-        print("""\n\n\nYour token has expired , create a new one : https://developer.spotify.com/console/get-several-tracks/
+        print("""\n\n
+                       Your token has expired , create a new one : https://developer.spotify.com/console/get-several-tracks/
               
                        If you refreshed / created your token within the last hour , make sure you have the good ID
               \n\n\n""")
@@ -107,6 +127,9 @@ except FileNotFoundError:
     
 # Clean and process data
 if done_getting:
+    print("\n\nSummary of the bad playlists : \n")
+    for i in bad_playlist:
+        print(i)
     with open("good.json","r") as f:
         liked = json.load(f)
     liked = pd.DataFrame(liked["audio_features"])
@@ -115,15 +138,22 @@ if done_getting:
         with open(f"bad{i}.json","r") as f:
             disliked = json.load(f)
         exec(f"bad{i} = pd.DataFrame(disliked['audio_features'][:25])")
-        exec(f'bad{i}["liked"] = [0] * 25')
+        try : 
+            exec(f'bad{i}["liked"] = [0] * 25')
+            done_cleaning = True
+        except ValueError:
+            print(f"\n\n{bad_playlist[i]} wasn't long enough. It has to be at least 25 songs long.")
+            break
+
+        
+        
+
+# Modelling 
+if done_cleaning:
     data = pd.concat([liked,bad1,bad2,bad3,bad4])
     data.drop(["type","id","uri","track_href","analysis_url"],axis=1,inplace=True)
     data = data.sample(frac=1)
     data.to_csv("data.csv",index=False)
-    done_cleaning = True
-
-# Modelling 
-if done_cleaning:
     print("\n\nCreating your model.....")
     X , y = data.drop("liked",axis=1) , data.liked
     model = RandomForestClassifier() # TODO #4 Add small hyperparameter tuning ( keep the default values in the grid to avoid any accuracy loss ! )
